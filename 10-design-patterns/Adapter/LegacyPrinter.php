dd5c4de377cd1b897c7f885ce78e5676b2056c35
Michael <michael_r_monteith@yahoo.com>
2025-12-22 11:41:48 -0500

    Add LegacyPrinter class for legacy printing functionality

---
+++ 10-design-patterns/Adapter/LegacyPrinter.php
@@ -0,0 +1,12 @@
+<?php
+// LegacyPrinter.php - Old incompatible class
+// Date created: 2025-12-22
+// Created by: Michael Monteith
+
+class LegacyPrinter
+{
+    public function oldPrint($text)
+    {
+        return "Legacy print: {$text}";
+    }
+}
