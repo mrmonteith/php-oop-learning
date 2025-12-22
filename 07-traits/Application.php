8365660b98d4f857ff06f2117abde1d19662f26e
Michael <michael_r_monteith@yahoo.com>
2025-12-22 08:59:45 -0500

    Create Application class using LoggerTrait

---
+++ 07-traits/Application.php
@@ -0,0 +1,11 @@
+<?php
+// Application.php - Uses LoggerTrait
+// Date created: 2025-12-22
+// Created by: Michael Monteith
+
+require_once "LoggerTrait.php";
+
+class Application
+{
+    use LoggerTrait;
+}
