a415dd1f223972aad8013eea84dff6e1eb9822e3
Michael <michael_r_monteith@yahoo.com>
2025-12-22 14:51:07 -0500

    Add User model for MVC mini framework

---
+++ 20-mvc-mini-framework/app/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+// User.php - Example model for MVC mini framework
+// Date created: 2025-12-22
+// Created by: Michael Monteith
+
+class User
+{
+    public $name = "Michael";
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
