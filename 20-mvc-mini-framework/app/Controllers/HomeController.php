af42691bcc0e4bebab149e739bb56f22e1d02e72
Michael <michael_r_monteith@yahoo.com>
2025-12-22 15:48:30 -0500

    Instantiate User model in HomeController index method
    Added User model instantiation and data extraction for view rendering.

--- 20-mvc-mini-framework/app/Controllers/HomeController.php
+++ 20-mvc-mini-framework/app/Controllers/HomeController.php
@@ -3,11 +3,21 @@
 // Date created: 2025-12-22
 // Created by: Michael Monteith
 
+require_once "../app/Models/User.php";
+
 class HomeController
 {
     public function index()
     {
+        // Create a model instance
+        $user = new User("Michael Monteith", "michael@example.com");
+
+        // Make $user available to the view
+        $data = ['user' => $user];
+
+        // Render the view
         ob_start();
+        extract($data);
         include "../app/Views/home.php";
         return ob_get_clean();
     }
