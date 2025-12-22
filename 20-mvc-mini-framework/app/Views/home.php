6d8e15c3b0a04f9ba51238b41dd7b85150eba3a5
Michael <michael_r_monteith@yahoo.com>
2025-12-22 14:39:27 -0500

    Add home.php view for MVC Mini Framework

---
+++ 20-mvc-mini-framework/app/Views/home.php
@@ -0,0 +1,6 @@
+<!-- home.php - Simple MVC view -->
+<!-- Date created: 2025-12-22 -->
+<!-- Created by: Michael Monteith -->
+
+<h1>MVC Mini Framework Example</h1>
+<p>This is a simple MVC view.</p>
