1ed3a9144fae70b3dddb0742fe27a4c12da4dfb0
Michael <michael_r_monteith@yahoo.com>
2025-12-22 08:46:15 -0500

    Create LoggerInterface for logging functionality

---
+++ 05-interfaces/Loggerinterface.php
@@ -0,0 +1,9 @@
+<?php
+// LoggerInterface.php - Demonstrates PHP interfaces
+// Date created: 2025-12-22
+// Created by: Michael Monteith
+
+interface LoggerInterface
+{
+    public function log($message);
+}
