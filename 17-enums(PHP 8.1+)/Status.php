fcfd84e0b03b16afd9eb6814955cb7b6a787e2b0
Michael <michael_r_monteith@yahoo.com>
2025-12-22 14:26:07 -0500

    Create Status enum in PHP 8.1
    Demonstrates the use of enums in PHP 8.1 with a Status enum.

---
+++ 17-enums(PHP 8.1+)/Status.php
@@ -0,0 +1,11 @@
+<?php
+// Status.php - Demonstrates PHP 8.1 enums
+// Date created: 2025-12-22
+// Created by: Michael Monteith
+
+enum Status
+{
+    case Pending;
+    case Active;
+    case Suspended;
+}
