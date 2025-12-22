7ad57bd58cebad9a79b8137306a4047c467889ae
Michael <michael_r_monteith@yahoo.com>
2025-12-22 15:48:56 -0500

    Display user name and email in home view

--- 20-mvc-mini-framework/app/Views/home.php
+++ 20-mvc-mini-framework/app/Views/home.php
@@ -3,4 +3,6 @@
 <!-- Created by: Michael Monteith -->
 
 <h1>MVC Mini Framework Example</h1>
-<p>This is a simple MVC view.</p>
+
+<p><strong>User Name:</strong> <?= htmlspecialchars($user->name) ?></p>
+<p><strong>Email:</strong> <?= htmlspecialchars($user->email) ?></p>
