da795dc363fd3a77d38c20c72f9274d748a3b426
Michael <michael_r_monteith@yahoo.com>
2025-12-22 14:16:36 -0500

    Create MathHelper class with static methods and properties

---
+++ 15-static-methods-properties/MathHelper.php
@@ -0,0 +1,14 @@
+<?php
+// MathHelper.php - Demonstrates static methods and properties
+// Date created: 2025-12-22
+// Created by: Michael Monteith
+
+class MathHelper
+{
+    public static $pi = 3.14159;
+
+    public static function square($n)
+    {
+        return $n * $n;
+    }
+}
