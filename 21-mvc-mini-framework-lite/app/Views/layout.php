0ae4a09d623294981463c3381f2757fce3fb4aa7
Michael <michael_r_monteith@yahoo.com>
2025-12-22 17:13:27 -0500

    Add main layout template for MVC framework

---
+++ 21-mvc-mini-framework-lite/app/Views/layout.php
@@ -0,0 +1,13 @@
+<!-- layout.php - Main layout template -->
+<!-- Date created: 2025-12-22 -->
+<!-- Created by: Michael Monteith -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lite MVC Framework</title>
+</head>
+<body>
+    <?= $content ?>
+</body>
+</html>
