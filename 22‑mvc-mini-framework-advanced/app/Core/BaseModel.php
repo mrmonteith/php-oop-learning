ab4e02fe12fdc900c8e551eb443b1820b2fc1e2a
Michael <michael_r_monteith@yahoo.com>
2025-12-23 09:30:57 -0500

    Add BaseModel class for Advanced MVC
    Created a base model class for the Advanced MVC framework.

---
+++ 22‑mvc-mini-framework-advanced/app/Core/BaseModel.php
@@ -0,0 +1,11 @@
+<?php
+// BaseModel.php - Base model for Advanced MVC
+// Date created: 2025-12-23
+// Created by: Michael Monteith
+
+namespace App\Core;
+
+class BaseModel
+{
+    // Placeholder for DB logic later
+}
