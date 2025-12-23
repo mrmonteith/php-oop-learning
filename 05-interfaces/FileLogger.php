b2ccdb1dae52d37c6fa8d8a202870e3f094e541a
Michael <michael_r_monteith@yahoo.com>
2025-12-23 14:08:39 -0500

    Update FileLogger implementation and metadata

--- 05-interfaces/FileLogger.php
+++ 05-interfaces/FileLogger.php
@@ -1,14 +1,14 @@
 <?php
 // FileLogger.php - Implements LoggerInterface
-// Date created: 2025-12-22
+// Date created: 2025-12-23
 // Created by: Michael Monteith
 
-require_once "LoggerInterface.php";
+require_once __DIR__ . "/LoggerInterface.php";
 
 class FileLogger implements LoggerInterface
 {
-    public function log($message)
+    public function log(string $message): void
     {
-        return "Logging to file: {$message}";
+        file_put_contents(__DIR__ . "/log.txt", $message . PHP_EOL, FILE_APPEND);
     }
 }
