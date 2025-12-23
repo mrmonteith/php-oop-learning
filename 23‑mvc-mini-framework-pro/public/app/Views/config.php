2bb1af5b0992ad2d5cda2ee888daf3e6b5105d23
Michael <michael_r_monteith@yahoo.com>
2025-12-23 15:53:27 -0500

    Add comments to config.php for clarity
    Added metadata comments for configuration file.

--- 23‑mvc-mini-framework-pro/public/app/Views/config.php
+++ 23‑mvc-mini-framework-pro/public/app/Views/config.php
@@ -1,4 +1,8 @@
 <?php
+// config.php - Application configuration
+// Date created: 2025-12-23
+// Created by: Michael Monteith
+
 return [
     'app_name' => 'Pro MVC Framework',
     'debug' => true,
