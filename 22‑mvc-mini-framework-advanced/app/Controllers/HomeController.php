a03c6e125faa77eddaabfdd4bb7a6ae33da2c481
Michael <michael_r_monteith@yahoo.com>
2025-12-23 09:31:39 -0500

    Add HomeController for handling home page requests

---
+++ 22‑mvc-mini-framework-advanced/app/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+// HomeController.php - Default controller for Advanced MVC
+// Date created: 2025-12-23
+// Created by: Michael Monteith
+
+namespace App\Controllers;
+
+use App\Core\BaseController;
+use App\Models\User;
+
+class HomeController extends BaseController
+{
+    public function index($request, $response)
+    {
+        $user = new User("Michael Monteith", "michael@example.com");
+        return $this->view('home', ['user' => $user]);
+    }
+}
