4afa639a2a7e26935677a3dc7b8a07b9d38b1165
Michael <michael_r_monteith@yahoo.com>
2025-12-23 15:31:52 -0500

    Add main layout template layout.php

---
+++ 23‑mvc-mini-framework-pro/public/app/Views/layout.php
@@ -0,0 +1,13 @@
+<!-- layout.php - Main layout template -->
+<!-- Date created: 2025-12-23 -->
+<!-- Created by: Michael Monteith -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pro MVC Framework</title>
+</head>
+<body>
+    <?= $content ?>
+</body>
+</html>
