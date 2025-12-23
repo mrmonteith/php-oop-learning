4d36434a28ccea10d613e17aaa31821d77e41296
Michael <michael_r_monteith@yahoo.com>
2025-12-23 09:46:41 -0500

    Add home view for Pro MVC Framework

---
+++ 23‑mvc-mini-framework-pro/app/Views/Views/home.php
@@ -0,0 +1,8 @@
+<!-- home.php - Home view -->
+<!-- Date created: 2025-12-23 -->
+<!-- Created by: Michael Monteith -->
+
+<h1>Welcome to the Pro MVC Framework</h1>
+
+<p><strong>Name:</strong> <?= htmlspecialchars($user->name) ?></p>
+<p><strong>Email:</strong> <?= htmlspecialchars($user->
