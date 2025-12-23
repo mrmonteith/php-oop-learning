306b76232708b364d91edd23f60006453e70fb17
Michael <michael_r_monteith@yahoo.com>
2025-12-23 09:33:19 -0500

    Add home view for Advanced MVC Framework

---
+++ 22‑mvc-mini-framework-advanced/app/Views/home.php
@@ -0,0 +1,8 @@
+<!-- home.php - Home view -->
+<!-- Date created: 2025-12-23 -->
+<!-- Created by: Michael Monteith -->
+
+<h1>Welcome to the Advanced MVC Framework</h1>
+
+<p><strong>Name:</strong> <?= htmlspecialchars($user->name) ?></p>
+<p><strong>Email:</strong> <?= htmlspecialchars($user->email) ?></p>
