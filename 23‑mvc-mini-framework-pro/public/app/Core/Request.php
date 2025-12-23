c9e5e0d3b825f38dc6d0d2cbd2360e6ea9f7ef40
Michael <michael_r_monteith@yahoo.com>
2025-12-23 15:28:53 -0500

    Add Request class for HTTP request handling

---
+++ 23‑mvc-mini-framework-pro/public/app/Core/Request.php
@@ -0,0 +1,29 @@
+<?php
+// Request.php - HTTP request abstraction
+// Date created: 2025-12-23
+// Created by: Michael Monteith
+
+namespace App\Core;
+
+class Request
+{
+    public function getPath()
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+
+    public function getMethod()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    public function get($key)
+    {
+        return $_GET[$key] ?? null;
+    }
+
+    public function post($key)
+    {
+        return $_POST[$key] ?? null;
+    }
+}
