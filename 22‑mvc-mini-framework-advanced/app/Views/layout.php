5981064ab3864f6ff8f26d4b5f453a47be3f68a0
Michael <michael_r_monteith@yahoo.com>
2025-12-23 09:32:35 -0500

    Add main layout template for MVC framework

---
+++ 22‑mvc-mini-framework-advanced/app/Views/layout.php
@@ -0,0 +1,13 @@
+<!-- layout.php - Main layout template -->
+<!-- Date created: 2025-12-23 -->
+<!-- Created by: Michael Monteith -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Advanced MVC Framework</title>
+</head>
+<body>
+    <?= $content ?>
+</body>
+</html>
