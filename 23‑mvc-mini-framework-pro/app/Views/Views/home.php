5a726b9561c5371a35bf9e1f52ece70a1611e58c
mrmonteith <michael_r_monteith@yahoo.com>
2025-12-23 15:25:46 -0500

    cleanup

--- 23‑mvc-mini-framework-pro/app/Views/Views/home.php
+++
@@ -1,8 +0,0 @@
-<!-- home.php - Home view -->
-<!-- Date created: 2025-12-23 -->
-<!-- Created by: Michael Monteith -->
-
-<h1>Welcome to the Pro MVC Framework</h1>
-
-<p><strong>Name:</strong> <?= htmlspecialchars($user->name) ?></p>
-<p><strong>Email:</strong> <?= htmlspecialchars($user->
