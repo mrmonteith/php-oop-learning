f80b8d4090f090e0988a325939682e6d435a003f
Michael <michael_r_monteith@yahoo.com>
2025-12-23 12:14:21 -0500

    Add HomeController with index method

---
+++ 40-php-testing/03-controller-tests/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+// HomeController.php - Example controller
+// Date created: 2025-12-23
+// Created by: Michael Monteith
+
+class HomeController
+{
+    public function index()
+    {
+        return "Hello from HomeController";
+    }
+}
