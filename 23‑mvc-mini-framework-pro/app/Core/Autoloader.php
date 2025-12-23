686882899a0e5371ccbdbb9cdd97c94954ad26bd
Michael <michael_r_monteith@yahoo.com>
2025-12-23 09:38:40 -0500

    Implement PSR-4 autoloader class

---
+++ 23‑mvc-mini-framework-pro/app/Core/Autoloader.php
@@ -0,0 +1,29 @@
+<?php
+// Autoloader.php - PSR-4 autoloader class
+// Date created: 2025-12-23
+// Created by: Michael Monteith
+
+namespace App\Core;
+
+class Autoloader
+{
+    public static function register()
+    {
+        spl_autoload_register([self::class, 'load']);
+    }
+
+    public static function load($class)
+    {
+        $prefix = 'App\\';
+        $baseDir = __DIR__ . '/../';
+
+        if (str_starts_with($class, $prefix)) {
+            $relative = substr($class, strlen($prefix));
+            $path = $baseDir . str_replace('\\', '/', $relative) . '.php';
+
+            if (file_exists($path)) {
+                require_once $path;
+            }
+        }
+    }
+}
