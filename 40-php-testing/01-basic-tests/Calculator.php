39ecab544e6f0d8abf3e280a8e421e09cbbdcb91
Michael <michael_r_monteith@yahoo.com>
2025-12-23 12:05:30 -0500

    Add Calculator class with basic arithmetic methods

---
+++ 40-php-testing/01-basic-tests/Calculator.php
@@ -0,0 +1,10 @@
+<?php
+// Calculator.php - Simple class for testing
+// Date created: 2025-12-23
+// Created by: Michael Monteith
+
+class Calculator
+{
+    public function add($a, $b) { return $a + $b; }
+    public function subtract($a, $b) { return $a - $b; }
+}
